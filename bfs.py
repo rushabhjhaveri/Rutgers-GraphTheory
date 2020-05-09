bacb2a83e6b37202756b6fd7380b6230f05d2834
rushabhjhaveri <rjhaveri98@gmail.com>
2020-05-09 19:17:32 -0400

    todo: comments

--- bfs.py
+++ bfs.py
@@ -1,1 +1,30 @@
 # Python code to implement breadth-first graph search algorithm.
+
+from collections import defaultdict
+
+class Graph: 
+    def __init__(self): 
+        self.graph = defaultdict(list)
+
+    def add_edge(self, vertex1, vertex2): 
+        self.graph[vertex1].append(vertex2)
+    
+    def BFS(self, source_vertex): 
+        graphlen = len(self.graph)
+        visited_vertices = [False] * (graphlen)
+
+        BFS_queue = []
+
+        BFS_queue.append(source_vertex)
+        visited_vertices[source_vertex] = True
+
+        while BFS_queue: 
+
+            source_vertex = BFS_queue.pop(0)
+
+            print(source_vertex, end=" ")
+
+            for neighbor in self.graph[source_vertex]: 
+                if visited_vertices[neighbor] == False:
+                    BFS_queue.append(neighbor)
+                    visited_vertices[neighbor] = True
--- bfsdriver.py
+++ bfsdriver.py
@@ -1,1 +1,18 @@
 # Driver code to execute the BFS graph search algorithm (implemented in python). 
+import bfs as bfs 
+
+graph = bfs.Graph()
+
+graph.add_edge(0, 1)
+
+graph.add_edge(0, 2)
+
+graph.add_edge(1, 2)
+
+graph.add_edge(2, 0)
+
+graph.add_edge(2, 3)
+
+graph.add_edge(3, 3)
+
+graph.BFS(2)
